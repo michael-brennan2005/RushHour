120f286cdbed8342d866151c24a60d3b26b27310
Michael Brennan <michael.brennancolorado@gmail.com>
2024-02-16 13:02:31 -0500

    stuff today

--- src/RushHour.java
+++ src/RushHour.java
@@ -1,9 +1,12 @@
+import javalib.funworld.World;
+import javalib.funworld.WorldScene;
 import javalib.worldimages.*;
 import tester.Tester;
 import java.awt.*;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
+// MARK: List definition
 interface IList<T> {
   // Applies the func to all elements in the list.
   <U> IList<U> map(Function<T, U> func);
@@ -64,6 +67,69 @@ class MT<T> implements IList<T> {
   }
 }
 
+// MARK: Drawing
+// Represents a tile to draw on the screen.
+class Tile {
+  int col;
+  int row;
+  Color color;
+
+  public Tile(int col, int row, Color color) {
+    this.col = col;
+    this.row = row;
+    this.color = color;
+  }
+
+  WorldImage draw(int tileSize) {
+    return new VisiblePinholeImage(
+            new RectangleImage(
+                    tileSize,
+                    tileSize,
+                    OutlineMode.SOLID,
+                    this.color
+            ).movePinhole(-0.5 * tileSize, -0.5 * tileSize)
+    );
+  }
+
+  WorldScene drawOntoScene(WorldScene scene, int tileSize) {
+    return scene.placeImageXY(this.draw(tileSize), this.col * tileSize, this.row * tileSize);
+  }
+}
+
+
+class DrawTilesOntoGrid implements BiFunction<Tile, WorldScene, WorldScene> {
+  int tileSize;
+  DrawTilesOntoGrid(int tileSize) {
+    this.tileSize = tileSize;
+  }
+
+  public WorldScene apply(Tile tile, WorldScene sceneSoFar) {
+    return tile.drawOntoScene(sceneSoFar, tileSize);
+  }
+}
+
+// Represents a collection of tiles to draw
+class TileGrid {
+  int cols;
+  int rows;
+  int tileSize;
+  IList<Tile> tiles;
+
+  WorldScene makeScene() {
+    WorldScene base = new WorldScene(
+            this.cols * this.tileSize,
+            this.rows * this.tileSize)
+            .placeImageXY(
+                    new RectangleImage(
+                            this.cols * this.tileSize,
+                            this.rows * this.tileSize,
+                            OutlineMode.SOLID,
+                            Color.WHITE),
+                    0,
+                    0);
+    return ;
+  };
+}
 class Vec2D {
   int x;
   int y;
@@ -77,7 +143,6 @@ class Vec2D {
     return this.y == that.y
             || this.x == that.x;
   }
-
 }
 
 class Vehicle {
@@ -116,9 +181,6 @@ class Vehicle {
   }
 
   public boolean overlaps(Vehicle that) {
-    //this vehicle can overlap that horizontally or vertically
-    //idea: make list of positions of this and that, contains on them
-    //if disjoint, return false
     return this.overlapsHelper(this.x1, this.y1, this.x2, this.y2,
             that.x1, that.y1, that.x2, that.y2);
   }
@@ -146,6 +208,11 @@ class Vehicle {
               && v2y1 >= v1y1 && v2y1 <= v1y2));
     }
   }
+
+  public boolean atLocation(int x, int y) {
+    return ((this.x1 == x && this.y1 == y)
+            || (this.x2 == x && this.y2 == y));
+  }
 }
 
 class PlaceVehiclesOntoImage implements BiFunction<Vehicle, WorldImage, WorldImage> {
@@ -169,39 +236,40 @@ class Grid {
     this.tileSize = tileSize;
   }
 
+  int totalWidth() {
+    return cols * tileSize;
+  }
+
+  int totalHeight() {
+    return rows * tileSize;
+  }
+
   // Produces an image of an empty grid.
   WorldImage toImage() {
-    WorldImage topAndBottomBorder = new RectangleImage(
-            this.cols * this.tileSize,
-            this.tileSize,
-            OutlineMode.SOLID,
-            Color.DARK_GRAY);
-    WorldImage leftAndRightBorder = new RectangleImage(
-            this.tileSize,
-            (this.rows - 2) * this.tileSize,
-            OutlineMode.SOLID,
-            Color.DARK_GRAY
-    );
-    WorldImage playArea = new RectangleImage(
-            (this.cols - 2) * this.tileSize,
-            (this.rows - 2) * this.tileSize,
-            OutlineMode.SOLID,
-            Color.WHITE
-    );
-
-    return new AboveImage(
-            topAndBottomBorder,
-            new BesideImage(
-                    leftAndRightBorder,
-                    playArea,
-                    leftAndRightBorder
-            ),
-            topAndBottomBorder
-    );
+    WorldImage base = new VisiblePinholeImage(
+            new RectangleImage(
+                    this.tileSize * this.cols,
+                    this.tileSize * this.rows,
+                    OutlineMode.SOLID,
+                    Color.WHITE)
+                    .movePinholeTo(
+                            new Posn(
+                                    (int)((this.tileSize * this.cols) / -2.0),
+                                    (int)((this.tileSize * this.rows) / -2.0))));
+    WorldImage addTopBorder = new PlaceImage(
+            base,
+            new RectangleImage(
+                    this.tileSize * this.cols,
+                    this.tileSize,
+                    OutlineMode.SOLID,
+                    Color.GRAY),
+            0,
+            0).toImage();
+    return addTopBorder;
   }
 }
 
-class RushHour {
+class RushHour extends World {
   IList<Vehicle> vehicles;
   Grid grid;
   //make an exit posn
@@ -215,7 +283,7 @@ class RushHour {
   make sure defined endx and endy are in the grid (helper method)
   string constructor:
   ermm find an arbitrary vehicle on same line as exit, set that as target vehicle
-  dont add to the vehicles list (consing in in reg constructor)
+  dont add to the vehicles list (consing in reg constructor)
   then find x and set those coords as endx endy
   then just wincheck by checking if any coordinate in target vehicle = endx endy
   1love
@@ -224,25 +292,48 @@ class RushHour {
   int endX;
   int endY;
 
+
   RushHour(IList<Vehicle> vehicles, Grid board, int tileSize,
            Vehicle targetVehicle, int endX, int endY) {
+    super();
     this.vehicles = vehicles;
     this.grid = board;
     this.tileSize = tileSize;
     this.utils = new RushHourUtils();
+    this.targetVehicle = targetVehicle;
+    this.endX = endX;
+    this.endY = endY;
   }
 
   RushHour(String level, RushHourUtils utils, int tileSize) {
     this(utils.getVehiclesList(level, level.length(),
                     0, 0, 0, new MT<Vehicle>(), tileSize),
             utils.getGrid(level, 0, 0, 0, tileSize),
-            tileSize);
+            tileSize,
+    new Vehicle(1, 1, 1, 1, Color.BLACK, tileSize),
+    //just set a random vehicle to target vehicle as of right now
+            /*
+            utils.getVehiclesList(level, level.length(),
+                    0, 0, 0, new MT<Vehicle>(), tileSize),
+             */
+            utils.getEndX(level, 0, 0, 0, tileSize),
+            utils.getEndY(level, 0, 0, 0, tileSize));
   }
 
   WorldImage toImage() {
     return this.vehicles
             .fold(new PlaceVehiclesOntoImage(), this.grid.toImage());
   }
+
+  public WorldScene makeScene() {
+    return new WorldScene(grid.totalWidth(), grid.totalHeight());
+  }
+
+  /*
+  public boolean winCheck() {
+  return this.targetVehicle.atLocation(this.endX, this.endY);
+  }
+   */
 }
 
 class RushHourUtils {
@@ -322,6 +413,32 @@ class RushHourUtils {
       return this.getGrid(level, i + 1, r + 1, 0, tileSize);
     }
   }
+
+  public int getEndX(String level, int i, int r, int currCol, int tileSize) {
+    String currLetter = level.substring(i, i + 1);
+    if (currLetter.equals("X")) {
+    return currCol;
+    } else if (currCol == 0
+            || (!currLetter.equals("+")
+            && !currLetter.equals("|"))) {
+      return this.getEndX(level, i + 1, r, currCol + 1, tileSize);
+    } else {
+      return this.getEndX(level, i + 1, r + 1, 0, tileSize);
+    }
+  }
+
+  public int getEndY(String level, int i, int r, int currCol, int tileSize) {
+    String currLetter = level.substring(i, i + 1);
+    if (currLetter.equals("X")) {
+      return r;
+    } else if (currCol == 0
+            || (!currLetter.equals("+")
+            && !currLetter.equals("|"))) {
+      return this.getEndY(level, i + 1, r, currCol + 1, tileSize);
+    } else {
+      return this.getEndY(level, i + 1, r + 1, 0, tileSize);
+    }
+  }
 }
 
 class PlaceImage {
@@ -339,10 +456,17 @@ class PlaceImage {
 
   WorldImage toImage() {
     return new OverlayImage(
-            this.base.movePinhole(
-                    (-1.0 * this.base.getWidth() / 2.0) + this.offsetX,
-                    (-1.0 * this.base.getHeight() / 2.0) + this.offsetY),
-            this.top).movePinholeTo(new Posn(0, 0));
+            this.base.movePinholeTo(
+                    new Posn(
+                            (int)((-1.0 * this.base.getWidth() / 2.0)),
+                            (int)((-1.0 * this.base.getHeight() / 2.0))))
+                    .movePinhole(this.offsetX, this.offsetY),
+            this.top.movePinholeTo(
+                    new Posn(
+                            (int)((-1.0 * this.top.getWidth() / 2.0)),
+                            (int)((-1.0 * this.top.getHeight() / 2.0))
+                    )
+            )).movePinholeTo(new Posn(0, 0));
   }
 }
 
@@ -357,8 +481,10 @@ class Main {
                             new MT<>())));
     Grid grid = new Grid(6, 6, 20);
 
-    RushHour game = new RushHour(vehicles, grid, 20);
-    game.toImage().saveImage("final.png");
+    Tile tile1 = new Tile(1,2,Color.RED);
+    tile1.drawOntoScene(new WorldScene(120,120), 10).saveImage("final.png");
+    //RushHour game = new RushHour(vehicles, grid, 20);
+    //grid.toImage().saveImage("final.png");
   }
 }
 
